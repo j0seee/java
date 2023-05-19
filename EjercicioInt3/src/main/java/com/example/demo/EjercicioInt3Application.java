70783ffe35d5e42865180da008a3d7e83f3af2fa
j0seee <131390344+j0seee@users.noreply.github.com>
2023-05-19 12:27:05 +0200

    Add files via upload

---
+++ EjercicioInt3/src/main/java/com/example/demo/EjercicioInt3Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioInt3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjercicioInt3Application.class, args);
+	}
+
+}
